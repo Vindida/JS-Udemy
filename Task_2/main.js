f99c42ef114b62a5baec192d9c0eeb7688f99c53
Alina <alina.kovalnko@gmail.com>
2019-11-28 21:35:25 +0300

    Задание 2. Практика.

---
+++ Task_2/main.js
@@ -0,0 +1,72 @@
+"use strict";
+
+let money = +prompt('Ваш бюджет на месяц?','');
+let time = +prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income:[],
+    savings: false
+  };
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Введите обязательную статью расходов в этом месяце',''),
+        b = prompt('Во сколько обойдется?','');
+
+    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
+        && a != '' && b != '' && a.length < 50){
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+      i = i - 1;
+    }
+};
+
+// let i = 0;
+// while (i < 2) {
+//   let a = prompt('Введите обязательную статью расходов в этом месяце',''),
+//   b = prompt('Во сколько обойдется?','');
+
+// if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
+//   && a != '' && b != '' && a.length < 50){
+//   console.log("done");
+//   appData.expenses[a] = b;
+// } else {
+      
+// }
+//  i++;
+// };
+
+// let i = 0;
+// do {
+//   let a = prompt('Введите обязательную статью расходов в этом месяце',''),
+//       b = prompt('Во сколько обойдется?','');
+
+//   if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
+//   && a != '' && b != '' && a.length < 50) {
+//   console.log("done");
+//   appData.expenses[a] = b;
+// } else {
+// } 
+// i++ }
+// while (i < 2);
+
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+  console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+  console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+  console.log("Высокий уровень достатка");
+} else {
+  console.log("Произошла ошибка");
+}
+
+

200679960a58844d45f288e44c3acf5960ae1e5b
Alina <alina.kovalnko@gmail.com>
2019-12-03 21:26:22 +0300

    Задание 5. Задачи на понимание основ JS.

---
+++ Task_5/main.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// let x = 5;
+// alert(x++);// 5
+ 
+// console.log([]+false-null+true);// NaN
+
+// console.log([]+1+2);// "12"
+
+// alert('1'[0]);// 1
+
+// console.log(2 && 1 && null && 0 && undefined);//  null
+
+// console.log(!!(a && b)) (a&&b)// разные
+
+// alert(null || 2&&3 || 4); // 3
+
+
+// alert(+Infinity); // infinity
+
+// "ёжик" > "яблоко" // верно
+
+// console.log(0 || "" || 2 || undefined || true || falsе); // 2
+

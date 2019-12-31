41100905bc9201c50e3fd15822594f2be8ba61ce
Alina <alina.kovalnko@gmail.com>
2019-12-31 16:51:58 +0300

    Задание 11. Создаем модальное окно

---
+++ Task_11/js_task.js
@@ -0,0 +1,11 @@
+
+
+
+
+let age = document.getElementById('age');
+
+function showUser(surname, name) {
+	alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+}
+showUser.apply(age, ["Горький","Максим"]);
+
